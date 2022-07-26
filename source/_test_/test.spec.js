89f315e3d0c048128c4f3bfd388545b022d04cab
James Ambat <jambat@snaplogic.com>
2022-07-25 22:45:34 -0700

    Revised test.spec.js

---
+++ source/_test_/test.spec.js
@@ -0,0 +1,7 @@
+describe("Testing our nodeJS app", () => {
+    it("Testing using Github Actions", () => {
+        // exoecting 3 functions and receiving 3
+        //  this test will pass unless the value change
+        expect(2).toBe(2);
+    });
+});
